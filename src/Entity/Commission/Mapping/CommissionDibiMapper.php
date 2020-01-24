229133d20991a0179483783bbb1266f98830e02e
Tomáš Krejčí <tomas@differencemakers.com>
2020-01-24 05:39:52 +0100

    ..

--- src/Entity/Commission/Mapping/CommissionDibiMapper.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Sellastica\Crm\Entity\Commission\Mapping;
-
-/**
- * @see \Sellastica\Crm\Entity\Commission\Entity\Commission
- */
-class CommissionDibiMapper extends \Sellastica\Entity\Mapping\DibiMapper
-{
-}
